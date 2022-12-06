22676c83743d3fe48a9c48ce1cc23876736b28b0
Talha <73413298+TalhaAmjad0034@users.noreply.github.com>
2022-12-06 14:33:00 +0500

    added mui for quick styling

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,16 @@
+import { Box } from "@mui/material";
 import React from "react";
 
 export const index: React.FunctionComponent = ({}) => {
   return (
     <React.Fragment>
-      <div>HomePage</div>
+      <Box
+        sx={{
+          color: "primary.main",
+        }}
+      >
+        HomePage
+      </Box>
     </React.Fragment>
   );
 };
